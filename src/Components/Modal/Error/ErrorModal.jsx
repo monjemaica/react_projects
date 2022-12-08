0723a46c1cad7876142adece172ac642e0ce06b6
monjemaica <jmon@obsequipment.com.au>
2022-12-08 10:49:14 +0800

    implement react portal

--- src/Components/Modal/Error/ErrorModal.jsx
+++ src/Components/Modal/Error/ErrorModal.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactDOM  from 'react-dom';
 import classname from './ErrorModal.module.css';
 import Card from '../../UI/Card/Card';
 import Button from '../../UI/Button/Button';
@@ -6,23 +7,40 @@ import Button from '../../UI/Button/Button';
 export const ErrorModal = (props) => {
 
     const closeModalHandler = (e) => {
-         props.onClose(true);
+        props.onClose(true);
     };
+    
+    const Backdrop = (props) => {
+        return <div className={classname.backdrop} onClick = {closeModalHandler} />
+    }
+    
+    const ModalOverlay = (props) => {
+        return <Card className={classname.modal}>
+    
+        <div className={classname.header}>
+            <h2>{props.err.title}</h2>
+        </div>
+        <div className={classname.content}>
+            <p>{props.err.message}</p>
+        </div>
+        <div className={classname.actions}>
+            <Button onClick={closeModalHandler}>Okay</Button>
+        </div>
+    </Card>
+    }
+ 
 
     return (
-        <div className={classname.backdrop}>
-            <Card className={classname.modal}>
-
-                <div className={classname.header}>
-                    <h2>{props.err.title}</h2>
-                </div>
-                <div className={classname.content}>
-                    <p>{props.err.message}</p>
-                </div>
-                <div className={classname.actions}>
-                    <Button onClick={closeModalHandler}>Okay</Button>
-                </div>
-            </Card>
-        </div>
+        <>
+            {ReactDOM.createPortal(
+                <Backdrop onClick={closeModalHandler} />, 
+                document.getElementById('backdrop-root')
+            )}
+            {ReactDOM.createPortal(
+                <ModalOverlay err={props.err} />,
+                document.getElementById('overlay-root')
+            )}
+        </>
+            
     )
 }
